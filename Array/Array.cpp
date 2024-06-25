5cfccb34a31b2e4868669d32bc49243a48ee08ec
김태훈 <hyri0510@gmail.com>
2024-06-26 00:50:44 +0900

    김태훈 : 자료구조 vector 구현

---
+++ Array/Array.cpp
@@ -0,0 +1,148 @@
+﻿// Array.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <crtdbg.h>
+#include <iostream>
+#include <vector>
+#include "MyVector.h"
+#include <string>
+
+template<typename DataType>
+void Print(DataType _Data)
+{
+	std::cout << _Data << std::endl;
+}
+
+void Print(const std::string& _Text);
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_CHECK_CRT_DF | _CRTDBG_ALLOC_MEM_DF);
+
+	if (true)
+	{
+		class Monster
+		{
+		public:
+			int mHp = 0;
+			
+		};
+
+		std::vector<Monster> Array(5);
+		Array.reserve(2);
+		Array.resize(1);
+
+		std::vector<Monster>::iterator StartIter = Array.begin();
+
+		int a = 0;
+	}
+
+	if (false)
+	{
+		std::cout << "========== resize, reserve 예제 ==========" << std::endl;
+
+		MyVector<int> Arr;
+
+		Print("reserve : 3");
+		Arr.reserve(3);
+		Display(Arr);
+
+		Print("reserve : 2");
+		Arr.reserve(2); 
+		Display(Arr);
+
+		Print("reserve : 7");
+		Arr.reserve(7); 
+		Display(Arr);
+
+		Print("reserve : 5");
+		Arr.resize(5);
+		Display(Arr);
+
+		Print("reserve : 3");
+		Arr.resize(3);
+		Display(Arr);
+
+		Print("reserve : 7");
+		Arr.resize(7);
+		Display(Arr);
+
+		int a = 0;
+	}
+
+	if (true)
+	{
+		std::cout << "========== push_back, pop_back 예제 ==========" << std::endl;
+
+		MyVector<int> Arr;
+
+		for (int i = 0; i < 10; i++)
+		{
+			Arr.push_back(i);
+			Display(Arr);
+		}
+
+		Print("Pop Back");
+		Arr.pop_back();
+		Display(Arr);
+		Print("Pop Back");
+		Arr.pop_back();
+		Display(Arr);
+		Print("Pop Back");
+		Arr.pop_back();
+		Display(Arr);
+		Print("Pop Back");
+		Arr.pop_back();
+		Display(Arr);
+	}
+
+	if (true)
+	{
+		std::cout << "========== push_front, pop_front 예제 ==========" << std::endl;
+
+		MyVector<int> Arr;
+
+		for (int i = 0; i < 10; i++)
+		{
+			Arr.push_front(i);
+			Display(Arr);
+		}
+
+		Arr.pop_front();
+		Display(Arr);
+		Arr.pop_front();
+		Display(Arr);
+		Arr.pop_front();
+		Display(Arr);
+		Arr.pop_front();
+		Display(Arr);
+	}
+
+	// 노드 개념을 추가하기 귀찮아서 미구현
+	if (false)
+	{
+		std::cout << "========== 반복자 구현 예제 ==========" << std::endl;
+
+		MyVector<int> Arr;
+
+
+		for (int i = 0; i < 10; i++)
+		{
+			Arr.push_back(i);
+		}
+
+		Display(Arr);
+
+		MyVector<int>::iterator StartIter = Arr.begin();
+		MyVector<int>::iterator EndIter = Arr.end();
+
+		for (; StartIter != EndIter; ++StartIter)
+		{
+		}
+	}
+}
+
+void Print(const std::string& _Text)
+{
+	std::cout << _Text << std::endl;
+}
---
+++ Array/MyVector.h
@@ -0,0 +1,329 @@
+#pragma once
+#include <assert.h>
+
+namespace my_math
+{
+	int max(int _v1, int _v2)
+	{
+		return _v1 < _v2 ? _v2 : _v1;
+	}
+
+	int min(int _v1, int _v2)
+	{
+		return _v1 < _v2 ? _v1 : _v2;
+	}
+}
+
+template<typename T>
+class MyVectorIterator
+{
+	template<typename T>
+	friend class MyVector;
+
+private:
+	using size_type = int;
+
+public:
+	MyVectorIterator()
+	{
+
+	}
+
+	MyVectorIterator(size_type _iSize)
+	{
+		mIdx = _iSize;
+	}
+
+	~MyVectorIterator()
+	{
+
+	}
+
+public:
+	bool operator==(const MyVectorIterator& _Other) const
+	{
+		return mIdx == _Other.mIdx;
+	}
+	bool operator!=(const MyVectorIterator& _Other) const
+	{
+		return mIdx != _Other.mIdx;
+	}
+
+	MyVectorIterator operator+(int _iValue)
+	{
+		return MyVectorIterator(mIdx + _iValue);
+	}
+
+	void operator++()
+	{
+		++mIdx;
+	}
+
+	void operator++(int)
+	{
+		++mIdx;
+	}
+
+private:
+	size_type mIdx = 0;
+
+};
+
+template<typename T>
+class MyVector
+{
+public:
+	using iterator = MyVectorIterator<T>;
+
+private:
+	using size_type = int;
+
+public:
+	MyVector()
+	{
+
+	}
+
+	MyVector(size_type _iSize)
+	{
+		if (_iSize <= 0)
+		{
+			// assert
+			return;
+		}
+
+		_ReAlloc(_iSize);
+	}
+
+	~MyVector()
+	{
+		// 이동 생성자 메모리 해제 방지
+		if (nullptr != mPtr)
+		{
+			delete[] mPtr;
+			mPtr = nullptr;
+		}
+	}
+
+	MyVector(const MyVector& _Other)
+	{
+		mCapacity = _Other.capacity();
+		mSize = _Other.size();
+		_ReAlloc(mCapacity);
+
+		for (size_type i = 0; i < mSize; i++)
+		{
+			mPtr[i] = _Other.mPtr[i];
+		}
+	}
+
+	MyVector(MyVector&& _Other) noexcept
+	{
+		mCapacity = _Other.capacity();
+		mSize = _Other.size();
+
+		mPtr = _Other.mPtr;
+		_Other.mPtr = nullptr;
+	}
+
+	MyVector& operator=(const MyVector& _Other)
+	{
+		mCapacity = _Other.capacity();
+		mSize = _Other.size();
+
+		_ReAlloc(mCapacity);
+
+		for (size_type i = 0; i < mSize; i++)
+		{
+			mPtr[i] = _Other.mPtr[i];
+		}
+
+		return *this;
+	}
+
+	MyVector& operator=(MyVector&& _Other)
+	{
+		mCapacity = _Other.capacity();
+		mSize = _Other.size();
+
+		mPtr= _Other.mPtr;
+		_Other.mPtr = nullptr;
+
+		return *this;
+	}
+
+public:
+	// Random Acess
+	T& operator[](size_type _Idx) const
+	{
+		if ((_Idx < 0) || (mSize < _Idx))
+		{
+			static T ErrorValue;
+			// assert
+			return ErrorValue;
+		}
+
+		return mPtr[_Idx];
+	}
+
+public:
+	inline size_type size() const { return mSize; }
+	inline size_type capacity() const { return mCapacity; }
+	inline void empty() const { return (mSize == 0); }
+
+	void reserve(size_type  _iCapacity)
+	{
+		mCapacity = my_math::max(mCapacity, _iCapacity);
+
+		_ReAlloc(mCapacity);
+	}
+
+	void resize(size_type  _iSize)
+	{
+		if (_iSize < 0)
+		{
+			// assert
+			return;
+		}
+
+		if (mSize < _iSize) // Realloc
+		{
+			_ReAlloc(_iSize);
+		}
+		else if (_iSize < mSize) // trim
+		{
+			for (size_type i = _iSize; i < mSize; i++)
+			{
+				mPtr[i] = NULL; 
+			}
+		}
+
+		mSize = _iSize;
+	}
+
+	void push_back(const T& _Data)
+	{
+		if (mCapacity == mSize)
+		{
+			_ReAlloc((mCapacity * 2) + 1);
+		}
+
+		mPtr[mSize] = _Data;
+
+		++mSize;
+	}
+
+	void pop_back()
+	{
+		if (mSize <= 0)
+		{
+			// assert
+			return;
+		}
+
+		mPtr[mSize - 1] = NULL;
+
+		--mSize;
+	}
+
+	// 배열을 앞에 항목을 삽입합니다.
+	void push_front(const T& _Data)
+	{
+		if (mCapacity == mSize)
+		{
+			_ReAlloc((mCapacity * 2) + 1);
+		}
+
+		for (size_type i = mSize - 1; 0 <= i; --i)
+		{
+			mPtr[i + 1] = mPtr[i];
+		}
+
+		mPtr[0] = _Data;
+
+		++mSize;
+	}
+
+	void pop_front()
+	{
+		if (mSize <= 0)
+		{
+			// assert
+			return;
+		}
+
+		mPtr[0] = NULL;
+
+		for (size_type i = 1; i < mSize; i++)
+		{
+			mPtr[i - 1] = mPtr[i];
+		}
+
+
+		--mSize;
+	}
+
+	void clear()
+	{
+		for (size_type i = 0; i < mSize; i++)
+		{
+			mPtr[i] = NULL;
+		}
+	}
+
+	iterator begin()
+	{
+		return iterator(0);
+	}
+
+	iterator end()
+	{
+		return iterator(mSize);
+	}
+
+private:
+	// 메모리 확장
+	void _ReAlloc(size_type _iCapacity)
+	{
+		// 요청한 배열이 더 작으면 리턴
+		if (_iCapacity < mCapacity)
+		{
+			return;
+		}
+
+		T* Temp = mPtr;
+		mPtr = new T[_iCapacity];
+		mCapacity = _iCapacity;
+
+		for (size_type  i = 0; i < mSize; i++)
+		{
+			mPtr[i] = Temp[i];
+		}
+
+		if (nullptr != Temp)
+		{
+			delete[] Temp;
+			Temp = nullptr;
+		}
+	}
+
+private:
+	size_type  mSize = 0;
+	size_type  mCapacity = 0;
+	T* mPtr = nullptr;
+
+};
+
+template<typename DataType>
+void Display(const MyVector<DataType>& _vec)
+{
+	std::cout << "Capacity : " << _vec.capacity() << "\n";
+	std::cout << "SIze : " << _vec.size() << "\n";
+
+	for (int i = 0; i < _vec.size(); i++)
+	{
+		std::cout << _vec[i] << ' ';
+	}
+
+	std::cout << '\n';
+}
